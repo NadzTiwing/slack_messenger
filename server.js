677a50001566552a5dd7368e6f8561d850dd2c1a
Nadia Tiwing <tiwingnadia@gmail.com>
2025-04-10 23:07:49 +0800

    export server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import fetch from 'node-fetch';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -36,5 +36,7 @@ app.post('/api/slack', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-}); 
+  console.log(`Server running at port:${port}`);
+});
+
+export default app;
